949d36c3d7c816fcd047da9dab01b9fce2414bdf
abdurrabb <mr.ab.watkins@gmail.com>
2021-10-02 22:38:09 -0400

    update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -2,28 +2,29 @@
 
 buildCharts(940);
 
-function buildCharts(_selected_id) {
+function buildCharts(selected_id) {
     d3.json("samples.json").then(data => {
         console.log(data);
         let ids = data.names;
         console.log("ids:", ids)
         let dropdown = d3.select("#selDataset");
         ids.forEach(id => {
-            dropdown.append("options").text(id)
+            dropdown.append("option").text(id)
         })
     
+        // let selected_id = 940;
         // SELECT DATA
-        let selected_id = selectData(selected_id, data);
+        let subjectData = selectData(selected_id, data);
         let metadata = subjectData.metadata;
         let samples = subjectData.samples;
     
         // DEMOGRAPHIC INFO
         meta_init(metadata);
-        
-        // CHARTS
+    
+        // HORIZONTAL BAR CHART
         bar_init(samples);
         bubble_init(samples);
-    })     
+    })
 }
 
 
@@ -31,20 +32,21 @@ function optionChanged(id) {
     console.log(id);
     buildCharts(id)
     // let selected_id = d3.select("#selDataset").text();
-
 }
 
-function selectData(id, data){
+function selectData(id, data) {
     let metadata = data.metadata.filter(subject => subject.id == id)[0];
-    let samples = data.samples.filter(subject => subject.id == id)[0]; 
+    let samples = data.samples.filter(subject => subject.id == id)[0];
     return {
-        metatdata: metadata, 
+        metadata: metadata, 
         samples: samples
     }
 }
 
 function meta_init(metadata) {
     meta_div = d3.select("#sample-metadata");
+    meta_div.html("")
+    // meta_div.append("p").text("Does this work?")
     Object.entries(metadata).forEach(([key, value]) => {
         meta_div.append("p").html(`<strong>${key.toUpperCase()}:</strong> ${value}`)
     })
@@ -53,11 +55,11 @@ function meta_init(metadata) {
 function bar_init(samples) {
     let trace = {
         x: samples.sample_values.slice(0,10).reverse(),
-        y: samples.otu_ids.map(id => `OTU ${id}`).slice(0,10).reverese(),
+        y: samples.otu_ids.map(id => `OTU ${id}`).slice(0,10).reverse(),
         type: "bar",
         orientation: "h"
     };
-    let traceData = [trace]    
+    let traceData = [trace]
     Plotly.newPlot("bar", traceData)
 }
 
@@ -65,15 +67,14 @@ function bubble_init(samples) {
     let trace = {
         x: samples.otu_ids,
         y: samples.sample_values,
-        text: samples.out_labels,
+        text: samples.otu_labels,
         mode: "markers",
         marker: {
-            size: samples.samples_values.map(value => value/2),
+            size: samples.sample_values.map(value => Math.log(value)*10),
             color: samples.otu_ids,
             colorscale: "YlGnBu"
         }
     };
-
     let traceData = [trace];
     Plotly.newPlot("bubble", traceData);
 }
